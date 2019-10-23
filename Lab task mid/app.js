63640f2df0cf3583ff83afe5a29d7993d5286840
Sohel Rana Sagor <sohelranasagor007@gmail.com>
2019-10-23 13:02:57 +0600

    2nd

--- Lab task mid/app.js
+++ Lab task mid/app.js
@@ -3,7 +3,9 @@ var bodyParser = require('body-parser');
 var ejs = require('ejs');
 var expSession = require('express-session');
 var mySql = require('mysql');
-var login = require('./controllers/login')
+var login = require('./controllers/login');
+var home = require('./controllers/home');
+var logout = require('./controllers/logout');
 var app = express();
 
 app.set('view engine','ejs');
@@ -11,6 +13,8 @@ app.set('view engine','ejs');
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(expSession({secret:"my top secret value", saveUninitialized:true, resave:false}));
 app.use('/login',login);
+app.use('/home',home);
+app.use('/logout',logout);
 
 app.listen(3000, function() {
     console.log('server started at 3000...');
